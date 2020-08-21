a80f78666e57638dceeafd28d73d67c39042ca57
Mattiew525 <69950976+Mattiew525@users.noreply.github.com>
2020-08-21 20:07:11 +0800

    Update insertion_sort.c

--- codes/insertion_sort.c
+++ codes/insertion_sort.c
@@ -1,19 +1,23 @@
 //Code from : https://github.com/TheAlgorithms/C/tree/master/sorting by Du Yuanchao
  
-void insertionSort(int *arr, int size)
+int insertionSort(int *arr, int size)
 {
 	int i;
+	int ctr = 0;
     for (i = 1; i < size; i++)
     {
         int j = i - 1;
         int key = arr[i];
+	ctr++;
         /* Move all elements greater than key to one position */
         while (j >= 0 && key < arr[j])
         {
             arr[j + 1] = arr[j];
             j = j - 1;
+	    ctr++;
         }
         /* Find a correct position for key */
         arr[j + 1] = key;
     }
+	return ctr;
 }
