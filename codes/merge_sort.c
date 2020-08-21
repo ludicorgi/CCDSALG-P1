3a83476a0c9294b6d10d8686f506a3bf58549f14
Mattiew525 <69950976+Mattiew525@users.noreply.github.com>
2020-08-21 20:12:18 +0800

    Update merge_sort.c

--- codes/merge_sort.c
+++ codes/merge_sort.c
@@ -4,7 +4,7 @@
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
+int merge(int arr[], int l, int m, int r) 
 { 
 	int i, j, k; 
 	int n1 = m - l + 1; 
