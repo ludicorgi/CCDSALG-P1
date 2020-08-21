f0168952c54d3c35660dc4bbe31bc27ca09e7b96
Mattiew525 <69950976+Mattiew525@users.noreply.github.com>
2020-08-21 17:23:23 +0800

    Update bubble_sort.c
    - added ctr variable
    - changed return type to int to return ctr

--- codes/bubble_sort.c
+++ codes/bubble_sort.c
@@ -5,27 +5,33 @@
  * @param arr array to be sorted
  * @param size size of array
  */
-void bubbleSort(int *arr, int size)
+int bubbleSort(int *arr, int size)
 {
 	int i,j;
+	int ctr = 0;
     for (i = 0; i < size - 1; i++)
     {                         /* for each array index */
         bool swapped = false; /* flag to check if any changes had to be made */
         /* perform iterations until no more changes were made or outer loop
             executed for all array indices */
+	    ctr++;
         for (j = 0; j < size - 1 - i; j++)
         { /* for each element in the array */
+		ctr++;
             if (arr[j] > arr[j + 1])
             { /* if the order of successive elements needs update */
                 swap(&arr[j], &arr[j + 1]);
                 swapped = true; /* set flag */
+		ctr++;
             }
         }
         if (!swapped)
         {
+		ctr++;
             /* since no more updates we made, the array is already sorted
                 this is an optimization for early termination */
             break;
         }
     }
+	return ctr;
 }
