8fed50c7a861069d34b1c6af221f265d7e159adc
Mattiew525 <69950976+Mattiew525@users.noreply.github.com>
2020-08-21 20:33:46 +0800

    Update shell_sort.c

--- codes/shell_sort.c
+++ codes/shell_sort.c
@@ -1,12 +1,23 @@
 //Code from https://github.com/TheAlgorithms/C/tree/master/sorting by Du Yuanchao
 
-void shellSort(int array[], int len)
+int shellSort(int array[], int len)
 {
     int i, j, gap;
-
+    int ctr = 0;
+    
     for (gap = len / 2; gap > 0; gap = gap / 2)
+    {
+        ctr++;
         for (i = gap; i < len; i++)
+        {
+            ctr++;
             for (j = i - gap; j >= 0 && array[j] > array[j + gap]; j = j - gap)
+            {   
+                ctr++;
                 swap(&array[j], &array[j + gap]);
+            }
+        }
+    }
+    return ctr;
 }
 
