fe26265d43a1efbf06186db6bf6099fadf18cee8
Mattiew525 <69950976+Mattiew525@users.noreply.github.com>
2020-08-21 20:31:56 +0800

    Update selection_sort.c

--- codes/selection_sort.c
+++ codes/selection_sort.c
@@ -5,22 +5,28 @@
  * @param arr array to be sorted
  * @param size size of array
  */
-void selectionSort(int *arr, int size)
+int selectionSort(int *arr, int size)
 {
 	int i, j;
+	int ctr = 0;
     for (i = 0; i < size - 1; i++)
     {
         int min_index = i;
+	ctr++;
         for (j = i + 1; j < size; j++)
         {
+		ctr++;
             if (arr[min_index] > arr[j])
             {
+		ctr++;
                 min_index = j;
             }
         }
         if (min_index != i)
         {
+	    ctr++;
             swap(arr + i, arr + min_index);
         }
     }
+	return ctr;
 }
